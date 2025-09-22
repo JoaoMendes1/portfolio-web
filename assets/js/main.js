7d5447a0b9734593d47dafea5399b55d7d0c3296
João Victor Mendes <joaodev21@gmail.com>
2025-09-21 22:08:03 -0300

    feat(js):3D TILT EFFECT ON CARDS

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ function handleTiltEffect() {
   if(window.innerWidth > 992) {
     //Inicializa o efeito de tilt em todos os cartões
     VanillaTilt.init(tiltcards, {
-      max: 10, 
+      max: 15, 
       speed: 400,
       glare: true, 
       "max-glare": 0.5,
